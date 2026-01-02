b0d76aacb0c7a321ab8e857320eb5b3336dc0ebd
Cookie-Panda <tyuiisgy@gmail.com>
2026-01-02 16:26:40 +0530

    update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
 const CACHE_NAME = 'm3e-player-v5';
 
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/scripts.js',
-  '/styles.css',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
-  '/jsmediatags.min.js',
-  '/sykg-zNym6YjUruM-QrEh7-nyTnjDwKNJ_190FjzaqkNCeE.woff2',
-  '/t5svIQcYNIWbFgDgAAzZ34auoVyXkJCOvp3SFWJbN5hF8Ju1x6sKCyp0l9sI40swNJwGpVd4AZzz0v6lJ4qFXNZhGjLvDSkV4W6GGn9Q3I8i.woff2'
+  '/M3E/',
+  '/M3E/index.html',
+  '/M3E/app.js',
+  '/M3E/scripts.js',
+  '/M3E/styles.css',
+  '/M3E/manifest.json',
+  '/M3E/icon-192.png',
+  '/M3E/icon-512.png',
+  '/M3E/jsmediatags.min.js',
+  '/M3E/sykg-zNym6YjUruM-QrEh7-nyTnjDwKNJ_190FjzaqkNCeE.woff2',
+  '/M3E/t5svIQcYNIWbFgDgAAzZ34auoVyXkJCOvp3SFWJbN5hF8Ju1x6sKCyp0l9sI40swNJwGpVd4AZzz0v6lJ4qFXNZhGjLvDSkV4W6GGn9Q3I8i.woff2'
 ];
 
 
